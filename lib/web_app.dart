e8f20822c9ce2ac3c033b71650ff250c0b635afd
lordyhas <dev.haspro@gmail.com>
2024-02-23 15:56:56 -0500

    WebApp and AndroidApp splited

---
+++ lib/web_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WebUIApp extends StatelessWidget {
+  const WebUIApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
