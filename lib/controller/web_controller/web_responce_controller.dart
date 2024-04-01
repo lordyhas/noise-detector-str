0920185112534a2ade7fd41be7dd8fd383057cc0
lordyhas <dev.haspro@gmail.com>
2024-03-31 22:08:51 -0400

    web ResponseController defined

--- lib/controller/web_controller/web_responce_controller.dart
+++ lib/controller/web_controller/web_responce_controller.dart
@@ -1,6 +1,10 @@
 
 
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/foundation.dart';
 
 class ResponseController {
   static const String refName = "detect_data";
@@ -8,5 +12,33 @@ class ResponseController {
   late final DatabaseReference _responseRef;
   final FirebaseDatabase database = FirebaseDatabase.instance;
 
-  ResponseController(): _responseRef = FirebaseDatabase.instance.ref("").child("response") ;
+  late StreamSubscription<DatabaseEvent> _receiveSubscription;
+
+
+  ResponseController(): _responseRef = FirebaseDatabase.instance
+      .ref(refName).child("web_response");
+
+  void listen({
+    required void Function(DatabaseEvent event) onMessageReceived,
+    void Function(FirebaseException exception)? onError,
+  }) async {
+    final responseQuery = _responseRef.limitToLast(1);
+
+    _receiveSubscription = responseQuery.onChildAdded.listen(
+          (DatabaseEvent event) => onMessageReceived(event),
+      onError: (Object o) {
+        final error = o as FirebaseException;
+        onError!(error);
+      },
+    );
+  }
+
+  void sendResponse(Map<String,String> map) async {
+    await _responseRef.push().set(map);
+  }
+
+  void close(){
+    _receiveSubscription.cancel();
+    //_counterSubscription.cancel();
+  }
 }
